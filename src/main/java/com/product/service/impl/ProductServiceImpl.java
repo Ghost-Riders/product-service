4750e93a9452b222497f4eaf6865c6e9a29fcec4
test <test@test.com>
2021-12-10 11:42:33 +0000

    mysql lib, transactional mgnt

--- src/main/java/com/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/product/service/impl/ProductServiceImpl.java
@@ -6,18 +6,22 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.product.model.Product;
 import com.product.repository.ProductRepository;
 import com.product.service.ProductService;
 
 @Service
+@Transactional
 public class ProductServiceImpl implements ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
 
 	@Override
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public Page<Product> ListOfProductsPropertyOrderByFromSize(String property, String orderBy, int from, int size) {
 		// property: column anme
 		// orderBy: ASC or DESC
